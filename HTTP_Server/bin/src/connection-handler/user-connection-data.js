f37f5d921548e647e95bce73ac178c6ae5e3382b
joshf67 <iownall99@live.co.uk>
2023-04-03 07:28:50 +0100

    Update user-connection-data.js
    Implemented new states for expecting data to work with message-builder

--- HTTP_Server/bin/src/connection-handler/user-connection-data.js
+++ HTTP_Server/bin/src/connection-handler/user-connection-data.js
@@ -1,13 +1,37 @@
+const { Document } = require("mongoose");
+
 /**
  * Stores any user related data
  */
 module.exports = class UserConnectionData {
+  //Stores a hashed version of the user's IP that randomizes every server restart
   ipHash = null;
+
+  lastMessageTime = null;
+  
+  //Store the last sent response from the user incase it needs to be resent
+  lastMessage = null;
+
+  //Store a function that allows the user to continue a response from multiple messages
+  expectingDataCallback = null;
+  clearExpectingDataCallback = null;
+  expectingDataState = null;
+
+  ResetExpectingDataCallback() {
+    if (this.clearExpectingDataCallback != null) this.clearExpectingDataCallback();
+    this.expectingDataCallback = null;
+    this.clearExpectingDataCallback = null;
+    this.expectingDataCallback = null;
+  }
+
+  //Stores the user's database login details 
   userHash = null;
   userHashData = null;
-  lastMessageTime = null;
-  lastMessage = null;
-  expectingData = null;
+
+  //Stores the user's mongoose database data
+  /**
+   * @type {Document}
+   */
   databaseData = null;
 
   /**
