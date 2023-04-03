faeb4f748ebb70aee0de7f9787f120ecf41792cc
joshf67 <iownall99@live.co.uk>
2023-04-03 07:29:16 +0100

    Updated response-types for new messages

--- HTTP_Server/bin/src/response-handler/response-types.js
+++ HTTP_Server/bin/src/response-handler/response-types.js
@@ -1,13 +1,22 @@
 const ResponseTypes = Object.freeze({
     None: -100,
-    Unexpected_Request: -4,
-    Type_Fail: -3,
-    Failed_To_Parse: -2,
+    Unexpected_Request: -99,
+    Type_Fail: -98,
+    Failed_To_Parse: -97,
+
+    Server_Error: -40,
+
+    User_Not_Logged_In: -2,
     Failed: -1,
     Succeeded: 0,
+
     Login_Updated: 1,
     Login_Failed: 2,
     Login_Complete: 3,
     Account_Creation_Complete: 4,
+
+    Item_Updated: 5,
+    Added_item: 6,
+    Removed_item: 7
 });
 module.exports.ResponseTypes = ResponseTypes;
