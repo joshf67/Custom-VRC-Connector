8c2acd1fba46a4a38aa1730b8177efd116ad4aa2
joshf67 <iownall99@live.co.uk>
2023-05-12 05:33:21 +0100

    Updated ServerResponseType folder location

--- Unity/Assets/Scripts/ServerConnector/Server/ServerResponseType.cs
+++ Unity/Assets/Scripts/ServerConnector/Server/ServerResponseType.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace ServerConnector.Response
+﻿namespace Joshf67.ServerConnector.Server
 {
     
 	public enum ServerResponseType
