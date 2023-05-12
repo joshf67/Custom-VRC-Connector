25f5b85050aedc758e6e351bfed30df278d78bbe
joshf67 <iownall99@live.co.uk>
2023-05-12 05:40:37 +0100

    Updated ServerExampleResponse to implement recent changes

--- Unity/Assets/Scripts/ServerConnector/Example/ServerExampleResponse.cs
+++ Unity/Assets/Scripts/ServerConnector/Example/ServerExampleResponse.cs
@@ -1,34 +1,81 @@
 ﻿
-using UdonSharp;
+using Joshf67.ServerConnector.Development;
 using UnityEngine;
-using VRC.SDKBase;
-using VRC.Udon;
+using VRC.SDK3.Data;
+using Joshf67.ServerConnector.Server;
 
-namespace ServerConnector.Response
+namespace Joshf67.ServerConnector.Example
 {
-	
-	public class ServerExampleResponse : ServerResponse
-	{
-		public object Response = "";
-		
-		override protected bool ParseResponse(object response) {
-			//Ensure Response is reset when a new parse happens
-			Response = "";
-			
-			if (Type == ServerResponseType.Login_Failed) {
-				Response = "Login credentials are invalid, please try again or create an account";
-			} else if (Type == ServerResponseType.Login_Updated) {
-				Response = UdonXML.GetNodeValue(response);
-			} else if (Type == ServerResponseType.Login_Complete) {
-				Response = UdonXML.GetNodeValue(UdonXML.GetChildNode(response, 1)).ToString();
-			} else if (Type == ServerResponseType.Added_Item) {
-				Response = UdonXML.GetNodeValue(UdonXML.GetChildNode(response, 1));
-			} else if (Type == ServerResponseType.Removed_Item) {
-				Response = UdonXML.GetNodeValue(UdonXML.GetChildNode(response, 1));
-			}
-			
-			return Response != null;
-		}
-	}
+
+    public class ServerExampleResponse : ServerResponse
+    {
+
+        /// <summary>
+        /// Handles the parsed message
+        /// </summary>
+        /// <param name="response"> The JSON DataDictionary to be handled </param>
+        /// <param name="HandleResult"> The result of the ServerRespons handler </param>
+        /// <returns> Boolean if the response was parsed properly </returns>
+        override protected bool HandleResponse(DataDictionary response, out DataToken HandleResult)
+        {
+            HandleResult = new DataToken(DataError.None);
+            bool messageWasParsedSucessfully = false;
+
+            if (Type == ServerResponseType.Login_Failed)
+            {
+                if (DevelopmentManager.IsServerResponseEnabled(DevelopmentMode.Basic))
+                    Debug.Log("ServerExampleResponse is handling a login failed type");
+
+                //The login has failed for some reason, so we need to display this to the user
+                HandleResult = new DataToken("Login credentials are invalid, please try again or create an account");
+            }
+            else if (Type == ServerResponseType.Login_Updated)
+            {
+                if (DevelopmentManager.IsServerResponseEnabled(DevelopmentMode.Basic))
+                    Debug.Log("ServerExampleResponse is handling a login update type");
+
+                //Server sends message with a string response so this will display the bits remaining until login completion
+                messageWasParsedSucessfully = response.TryGetValue("response", TokenType.String, out HandleResult);
+            }
+            else if (Type == ServerResponseType.Login_Complete)
+            {
+                if (DevelopmentManager.IsServerResponseEnabled(DevelopmentMode.Basic))
+                    Debug.Log("ServerExampleResponse is handling a login complete type");
+
+                messageWasParsedSucessfully = response.TryGetValue("response", TokenType.DataDictionary, out HandleResult);
+            }
+            else if (Type == ServerResponseType.Account_Creation_Complete)
+            {
+                if (DevelopmentManager.IsServerResponseEnabled(DevelopmentMode.Basic))
+                    Debug.Log("ServerExampleResponse is handling a account creation complete type");
+
+                messageWasParsedSucessfully = response.TryGetValue("response", TokenType.DataDictionary, out HandleResult);
+            }
+            else if (Type == ServerResponseType.Item_Updated)
+            {
+                if (DevelopmentManager.IsServerResponseEnabled(DevelopmentMode.Basic))
+                    Debug.Log("ServerExampleResponse is handling a item update type");
+
+                //Server sends message with a string response so this will display the bits remaining until item adding/remove message completion
+                messageWasParsedSucessfully = response.TryGetValue("response", TokenType.String, out HandleResult);
+            }
+            else if (Type == ServerResponseType.Added_Item)
+            {
+                if (DevelopmentManager.IsServerResponseEnabled(DevelopmentMode.Basic))
+                    Debug.Log("ServerExampleResponse is handling a Added item type");
+
+                messageWasParsedSucessfully = response.TryGetValue("response", TokenType.DataList, out HandleResult);
+            }
+            else if (Type == ServerResponseType.Removed_Item)
+            {
+                if (DevelopmentManager.IsServerResponseEnabled(DevelopmentMode.Basic))
+                    Debug.Log("ServerExampleResponse is handling a Removed item type");
+
+                messageWasParsedSucessfully = response.TryGetValue("response", TokenType.DataList, out HandleResult);
+            }
+
+            return !messageWasParsedSucessfully;
+        }
+    }
 
 }
