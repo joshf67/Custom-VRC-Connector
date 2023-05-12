a8d49279389ed27a421caac797eff6bcabb7352c
joshf67 <iownall99@live.co.uk>
2023-05-12 09:16:57 +0100

    Update message-unpacker.js
    Updated to handle new method of the starting bit being the control of if the type is included in the message

--- HTTP_Server/bin/src/message-handler/message-unpacker.js
+++ HTTP_Server/bin/src/message-handler/message-unpacker.js
@@ -7,25 +7,30 @@ const URLMessage = require("./url-message");
  * @returns {URLMessage} - The parsed message
  */
 module.exports.UnpackHexMessage = function UnpackHexMessage(message) {
+  //Convert the hex URL into the binary representation
   messageBitArray = parseInt(message, 16).toString(2).split("");
 
-  //Pack message to be the same length as MESSAGE_BITS_LENGTH to combat 0s being removed from end
-  messageBitArray = Array(
-    process.env.MESSAGE_BITS_LENGTH - messageBitArray.length
-  )
-    .fill("0")
-    .concat(messageBitArray);
-
-  //The first bit is used to determine if the type was sent along with the message
+  //If the type is included then parse out the message and the type
+  //To determine if the message includes the type we take
+  // the first bit as it is always provided with any message as
+  // either 1 or 0 due to the packing method on VRC side
   if (messageBitArray[0] == "1") {
     return new URLMessage(
-      messageBitArray.slice(process.env.MESSAGE_TYPE_BITS + 1),
+      messageBitArray.slice(parseInt(process.env.MESSAGE_TYPE_BITS) + 1),
       parseInt(
-        messageBitArray.slice(1, process.env.MESSAGE_TYPE_BITS + 1).join(""),
+        messageBitArray
+          .slice(1, parseInt(process.env.MESSAGE_TYPE_BITS) + 1)
+          .join(""),
         2
       )
     );
   } else {
-    return new URLMessage(messageBitArray.slice(1));
+    //Pack message to be the same length as MESSAGE_BITS_LENGTH to
+    // combat 0s being removed from end and remove 1 bit for no type included
+    return new URLMessage(
+      Array(
+        parseInt(process.env.MESSAGE_BITS_LENGTH) - messageBitArray.length - 1
+      ).fill("0").concat(messageBitArray)
+    );
   }
 };
